4cb5612ed2555a8a343915c81adc7a535a9cbdb1
jimao <jimao@jimaodeMacBook-Pro.local>
2023-07-24 17:49:15 +0800

    常量和结构体复制过来

--- tulipx.c
+++ tulipx.c
@@ -3,6 +3,28 @@
 #include "candles.h"
 #include "indicators.h"
 
+#define TASK_MAX 1000
+#define DATA_MAX 4
+
+typedef struct {
+  int enabled;
+  int target_index;
+  int is_outputs;
+  int data_index;
+} MapInfo;
+
+typedef struct {
+  int used;
+  int indicator_index;
+  int size;
+  TI_REAL options[DATA_MAX];
+  int inputs_offset;
+  TI_REAL * inputs[DATA_MAX];
+  int outputs_offset;
+  TI_REAL * outputs[DATA_MAX];
+  MapInfo inputs_map[DATA_MAX];
+} Task;
+
 int main() {
   printf("你好，世界\n");
   return 0;
